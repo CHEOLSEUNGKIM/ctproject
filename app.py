d6043facb741ba27147a14e04270d38b55d1710c
CHEOLSEUNGKIM <gochuzang117@gmail.com>
2025-07-28 13:57:56 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,41 @@
+import streamlit as st
+st.set_page_config(layout="wide")
+import streamlit.components.v1 as htmlviewer
+
+# Title Msg#1
+st.title('배드민턴 팀 스트로크 루틴 만들기')
+
+with open('./index.html', 'r', encoding='utf-8') as f:
+    html = f.read()
+    f.close()
+
+# html = '''
+# <html>
+#     <head>
+#         <title> this is my html </title>
+#     </head>
+
+#     <body>
+#         <h1>Topic</h1>
+#         <h2>SubTopic</h2>
+#     </body>
+# </html>
+# '''
+# Box#1(4), Box#2(1)
+col1, col2 = st.columns((4,1))
+with col1:
+    with st.expander('content #1...'):
+        url = 'https://www.youtube.com/watch?v=R5AbqmdVo6w'
+        st.info('배드민턴 복식 로테이션을 알아보자!')
+        st.video(url)
+    
+    with st.expander('content #2...'):
+        #st.write(html, unsafe_allow_html=True)
+        htmlviewer.html(html, height=800)
+        
+with col2:
+    with st.expander('Tips..'):
+        st.info('Tips..')
+
+st.markdown('<hr>', unsafe_allow_html=True)
+st.write('<font color="BLUE">(c)copyright. all rights reserved by CHEOLSEUNG KIM', unsafe_allow_html=True)
